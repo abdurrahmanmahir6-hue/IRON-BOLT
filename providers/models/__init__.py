02869f84110b34100a32ae4e86c14ed39b5970f5
abdurrahmanmahir6-hue <abdurrahmanmahir6@gmail.com>
2026-08-02 12:43:22 +0600

    Update __init__.py

--- providers/models/__init__.py
+++ providers/models/__init__.py
@@ -1,4 +1,4 @@
-from models.provider_config import ProviderConfig  
+from .provider_config import ProviderConfig  
 from .provider_request import ProviderRequest  
 from .provider_response import ProviderResponse  
 from .provider_capability import Capability, ProviderCapability  
